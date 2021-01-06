e3c89c3a0e7ea7898f9aff3b10fe7dc5b231dc55
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-01-05 19:42:55 -0800

    Add files via upload

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+
+import Grid from '@material-ui/core/Grid';
 
 import Product from './Product/Product';
-import useStyles from './styles';
+
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5' },
     { id: 2, name: "MacBook", description: 'Apple macbook', price: '$10' },
@@ -10,18 +11,17 @@ const products = [
 ];
 
 const Products = () => {
-    const classes = useStyles();
+
     return (
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
-                    </Grid>
-                ))}
-            </Grid>
-        </main>
+
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
+        </Grid>
+
     );
 };
 

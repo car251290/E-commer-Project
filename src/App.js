18a2ae289bf135c065c2ee96fa1b2e75a9416ced
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-01-19 20:43:05 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Navbar, Products} from './components';
-
+import {commerce} from './lib/commerce'
 
 const App = () => {
+    const [products,setProducts] = useState([]);
+
+    const fetchProduct = async () => {
+        const {data} = await commerce.products.list()
+        //asking the dat responce
+        setProducts(data)
+    }
+
+    useEffect(()=>{
+        fetchProduct();
+
+    },[]);
+
+    console.log(products);
+
     return (
         <div>
-
         <Navbar/>
          <Products/>
-
-         <Products/>
-
         </div>
     )
 }

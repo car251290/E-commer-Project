90c1800b42fa80cb7879c3333c423fc9424981cc
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-01-25 19:12:03 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ const App = () => {
     const [products,setProducts] = useState([]);
 
     const fetchProduct = async () => {
+
+        //how you ansync the class commerce to display the products
+
         const {data} = await commerce.products.list()
         //asking the dat responce
         setProducts(data)
     }
 
+//Hooks option to use the effect of the fect data 
     useEffect(()=>{
         fetchProduct();
 
     },[]);
 
+    //console.log(products);
     console.log(products);
-
     return (
         <div>
         <Navbar/>
